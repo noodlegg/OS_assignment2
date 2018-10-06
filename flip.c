db156ac00f4ff2807f2708b8c8c94c0d102ff181
deny33 <dennisrizvic3300@gmail.com>
2018-10-06 16:38:53 +0200

    Update flip.c
    my sht

--- flip.c
+++ flip.c
@@ -20,9 +20,43 @@
 
 #include "uint128.h"
 #include "flip.h"
+// create a bitmask where bit at position n is set
+#define BITMASK(n)          (((uint128_t) 1) << (n))
+// check if bit n in v is set
+#define BIT_IS_SET(v,n)     (((v) & BITMASK(n)) == BITMASK(n))
+// set bit n in v
+#define BIT_SET(v,n)        ((v) =  (v) |  BITMASK(n))
+// clear bit n in v
+#define BIT_CLEAR(v,n)      ((v) =  (v) & ~BITMASK(n))
+// declare a mutex, and it is initialized as well
+static pthread_mutex_t      mutex          = PTHREAD_MUTEX_INITIALIZER;
+
+
+int bit_test (uint128_t v){
+// set all bits to 1
+v = ~0;
+printf ("v (all 1's) : %lx%016lx\n", HI(v), LO(v));
+return v;
+}
+
+static void flipper(uint128_t v, int x){
+
+if(BIT_IS_SET(v,x)){
+BIT_CLEAR(v,x);
+}
+else{
+BIT_SET(v,x);
+}
+
+}
 
 int main (void)
 {
+uint128_t     v;
+bit_test;
+for(int x=0;x<HI(v);x++){
+flipper(v,x);
+}
     // TODO: start threads to flip the pieces and output the results
     // (see thread_test() and thread_mutex_test() how to use threads and mutexes,
     //  see bit_test() how to manipulate bits in a large integer)
@@ -30,4 +64,3 @@ int main (void)
 
     return (0);
 }
-
